175167cc7613eff65e6d34b0dec62505921cc3e5
madsray <30870145+madsray@users.noreply.github.com>
2018-02-25 19:05:53 -0600

    styled buttons

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ $( () => {
       $( "#left" ).css("transform","translateX(0)").css("opacity","1");
       $( "#right" ).css("transform","translateX(0)").css("opacity","1");
       $('#welcome').text("Are You Ready To See Your New Home?")
-      });
-
+    });
+//     const $suc = $('.theImg');
+// const makeBox = ()=>{
+//
+//
+// for (let i = 0; i < 99; i++){
+//   $('.succulent').prepend('<img class="theImg" src="imgs/succulent.png" />')
+// }
+// }
+// makeBox();
+//
+// // $suc.on('mouseout', () => {
+// //     $suc.css('border', '1px solid black');
+// //   });
 });
